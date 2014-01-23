5bb3d8a4715034af56dca7f1667dc519a13e9543
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2014-01-22 21:18:49 -0800

    Edited HTML & CSS to import styles from existing site.

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131015224246) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "concept_descriptions", force: true do |t|
     t.text     "description"
     t.integer  "concept_id"
